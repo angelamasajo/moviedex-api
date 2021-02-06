3338331a6ee7ae086897f3b4c1f68f4aef989323
Angela Masajo <aabmasajo@gmail.com>
2021-02-06 11:11:02 -0600

    added helmet

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const helmet = require('helmet')
 const cors = require('cors')
 const MOVIES = require('./movies-small.json')
 
 const app = express()
 
 app.use(morgan('dev'))
+app.use(helmet())
 app.use(cors())
 
 app.use(function validateBearerToken(req, res, next) {
